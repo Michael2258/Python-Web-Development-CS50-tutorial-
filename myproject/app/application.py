17381865e66051b3300904ce7c7f517add3bf647
Chuchi Shushi <thanh2t2219@yahoo.com>
2020-03-17 04:07:05 +0700

    link to file html to each other in Python file

--- myproject/app/application.py
+++ myproject/app/application.py
@@ -7,5 +7,10 @@ def index():
   names = ["Irene", "Seulgi", "Wendy", "Joy", "Yeri"]
   return render_template("index.html", names=names)
 
+@app.route("/more")
+def more():
+  others = ["Suho", "Taeyeon", "Sakura"]
+  return render_template("more.html", others=others)
+
 if __name__ == "__main__":
     app.run()
