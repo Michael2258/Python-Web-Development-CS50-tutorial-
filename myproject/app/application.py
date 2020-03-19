57ead62b6a3cdd7ef95208256595dd081f8392ea
Chuchi Shushi <thanh2t2219@yahoo.com>
2020-03-19 16:31:52 +0700

    add note using session, save your own data to the web

--- myproject/app/application.py
+++ myproject/app/application.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-  return render_template("index.html")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route("/hello", methods=["POST"]) # chỉ có thể truy cập vào route bằng code, thay vì gõ trên link như GET #
-def hello():
-  name = request.form.get("name")
-  return render_template("hello.html", name=name)
+notes = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+  if request.method == "POST":
+    note = request.form.get("note")
+    notes.append(note)
+
+  return render_template("index.html", notes=notes)
 
 if __name__ == "__main__":
-    app.run()
+    app.run() 
